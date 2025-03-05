b0c8a83518b0c6c7f97aad41290bda3c485171e6
ayyanovia1121 <noviayya1121@gmail.com>
2025-03-05 16:23:47 +0700

    feat(context): 🔧 add types app on appcontext

--- src/context/AppContex.tsx
+++ src/context/AppContex.tsx
@@ -1,24 +1,65 @@
-'use client'
-import { createContext, useContext } from "react";
+"use client";
+import { AppType, MenuItem } from "@/types/AppTypes";
+import { createContext, useContext, useState } from "react";
+import { LuLayoutDashboard } from "react-icons/lu";
+import { MdHistory, MdSubscriptions } from "react-icons/md";
+import { TbTemplate } from "react-icons/tb";
 
-// app context type
-interface AppType {}
 
 // use to set initial state
-const defaultState={};
+const defaultState = {
+  mainMenuItemsObject: {
+    mainMenuItems: [],
+    setMainMenuItems: () => {},
+  },
+};
 
 // create context with initial state to hold value of apptype
 const AppContext = createContext<AppType>(defaultState);
 
 // define context provider for app context
-export default function AppContextProvider ({
-    children,
-}:{
-    children: React.ReactNode;
+export default function AppContextProvider({
+  children,
+}: {
+  children: React.ReactNode;
 }) {
-    return <AppContext.Provider value={{}}>{children}</AppContext.Provider>
+
+    const [mainMenuItems, setMainMenuItems] = useState<MenuItem[]>([
+        {
+            icon: LuLayoutDashboard,
+            label: "Dashboard",
+            isSelected: true, 
+        },
+        {
+            icon: MdHistory,
+            label: "History",
+            isSelected: false, 
+        },
+        {
+            icon: MdSubscriptions,
+            label: "Subscriptions",
+            isSelected: false, 
+        },
+        {
+            icon: TbTemplate,
+            label: "Templates",
+            isSelected: false,
+        }
+    ]);
+  return (
+    <AppContext.Provider
+      value={{
+        mainMenuItemsObject: {
+          mainMenuItems,
+          setMainMenuItems,
+        },
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
 }
 
 export function useAppContext() {
-    return useContext(AppContext);
+  return useContext(AppContext);
 }
